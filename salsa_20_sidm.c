9b8a0546fed2254d15a8c9f6d284068620db3e90
Gerko de Roo <gerko.deroo@kangaderoo.nl>
2013-12-02 20:38:24 +0100

    Debug work in progress, this file is not used at the moment.

--- salsa_20_sidm.c
+++ salsa_20_sidm.c
@@ -27,30 +27,34 @@
 #include <immintrin.h>
 #include "cpuminer-config.h"
 #include "miner.h"
+#include <string.h>
 #include <stdlib.h>
 #include <inttypes.h>
 
 #if defined(__x86_64__)
 
-static inline void xor_salsa_sidm(__m128i *calc_18, __m128i  *calc_13,__m128i  *calc_9, __m128i *calc_7,
-  								  __m128i *calc_1,  __m128i  *calc_4,__m128i  *calc_3, __m128i *calc_2)
+//static inline void xor_salsa_sidm(__m128i *calc_18, __m128i  *calc_13, __m128i  *calc_9, __m128i *calc_7,
+ // 								  __m128i *calc_1,  __m128i   *calc_4, __m128i  *calc_3, __m128i *calc_2)
+
+static inline void xor_salsa_sidm(__m128i *calc_18, __m128i  *calc_7, __m128i  *calc_9, __m128i *calc_13,
+  								  __m128i *calc_1,  __m128i  *calc_2, __m128i  *calc_3, __m128i *calc_4)
 {
 	int i;
 	__m128i _calc;
 	__m128i _shift_left;
-	__m128i row1;
-	__m128i row2;
-	__m128i row3;
-	__m128i row4;
+	__m128i row1 = _mm_xor_si128(*calc_18, *calc_1);;
+	__m128i row2 = _mm_xor_si128(*calc_7, *calc_2);;
+	__m128i row3 = _mm_xor_si128(*calc_9, *calc_3);;
+	__m128i row4 = _mm_xor_si128(*calc_13, *calc_4);;
 
 	*calc_18 = _mm_xor_si128(*calc_18, *calc_1);
 	*calc_7 = _mm_xor_si128(*calc_7, *calc_2);
 	*calc_9 = _mm_xor_si128(*calc_9, *calc_3);
 	*calc_13 = _mm_xor_si128(*calc_13, *calc_4);
-	row1 = *calc_18;
-	row2 = *calc_7;
-	row3 = *calc_3;
-	row4 = *calc_13;
+//	row1 = *calc_18;
+//	row2 = *calc_7;
+//	row3 = *calc_9;
+//	row4 = *calc_13;
 
 	for (i = 0; i < 8; i += 2) {
 		/* first row */
@@ -219,4 +223,84 @@ static inline void scrypt_core_sidm(__m128i *X /*, uint32_t *V*/)
 
 #endif
 
+static inline void xor_salsa8_org(uint32_t B[16], const uint32_t Bx[16])
+{
+	uint32_t x00,x01,x02,x03,x04,x05,x06,x07,x08,x09,x10,x11,x12,x13,x14,x15;
+	int i;
+
+	x00 = (B[ 0] ^= Bx[ 0]);
+	x01 = (B[ 1] ^= Bx[ 1]);
+	x02 = (B[ 2] ^= Bx[ 2]);
+	x03 = (B[ 3] ^= Bx[ 3]);
+	x04 = (B[ 4] ^= Bx[ 4]);
+	x05 = (B[ 5] ^= Bx[ 5]);
+	x06 = (B[ 6] ^= Bx[ 6]);
+	x07 = (B[ 7] ^= Bx[ 7]);
+	x08 = (B[ 8] ^= Bx[ 8]);
+	x09 = (B[ 9] ^= Bx[ 9]);
+	x10 = (B[10] ^= Bx[10]);
+	x11 = (B[11] ^= Bx[11]);
+	x12 = (B[12] ^= Bx[12]);
+	x13 = (B[13] ^= Bx[13]);
+	x14 = (B[14] ^= Bx[14]);
+	x15 = (B[15] ^= Bx[15]);
+	for (i = 0; i < 8; i += 2) {
+#define R(a, b) (((a) << (b)) | ((a) >> (32 - (b))))
+//		/* Operate on columns. */
+		/* 0,4,8,12        *//* 1,5,9,13           *//* 2,6,10,14          *//* 3,7,11,15 */
+		x04 ^= R(x00+x12, 7);	x09 ^= R(x05+x01, 7);	x14 ^= R(x10+x06, 7);	x03 ^= R(x15+x11, 7);
+
+		x08 ^= R(x04+x00, 9);	x13 ^= R(x09+x05, 9);	x02 ^= R(x14+x10, 9);	x07 ^= R(x03+x15, 9);
+
+		x12 ^= R(x08+x04,13);	x01 ^= R(x13+x09,13);	x06 ^= R(x02+x14,13);	x11 ^= R(x07+x03,13);
+
+		x00 ^= R(x12+x08,18);	x05 ^= R(x01+x13,18);	x10 ^= R(x06+x02,18);	x15 ^= R(x11+x07,18);
+//
+//		/* Operate on rows. */
+//		/* 1,2,3,0         *//* 6,7,4,5            *//* 11,8,9,10          *//* 12,13,14,15 */
+		x01 ^= R(x00+x03, 7);	x06 ^= R(x05+x04, 7);	x11 ^= R(x10+x09, 7);	x12 ^= R(x15+x14, 7);
+//
+		x02 ^= R(x01+x00, 9);	x07 ^= R(x06+x05, 9);	x08 ^= R(x11+x10, 9);	x13 ^= R(x12+x15, 9);
+//
+		x03 ^= R(x02+x01,13);	x04 ^= R(x07+x06,13);	x09 ^= R(x08+x11,13);	x14 ^= R(x13+x12,13);
+//
+		x00 ^= R(x03+x02,18);	x05 ^= R(x04+x07,18);	x10 ^= R(x09+x08,18);	x15 ^= R(x14+x13,18);
+
+#undef R
+	}
+	B[ 0] += x00;
+	B[ 1] += x01;
+	B[ 2] += x02;
+	B[ 3] += x03;
+	B[ 4] += x04;
+	B[ 5] += x05;
+	B[ 6] += x06;
+	B[ 7] += x07;
+	B[ 8] += x08;
+	B[ 9] += x09;
+	B[10] += x10;
+	B[11] += x11;
+	B[12] += x12;
+	B[13] += x13;
+	B[14] += x14;
+	B[15] += x15;
+}
+
+static inline void scrypt_core_org(uint32_t *X, uint32_t *V)
+{
+	uint32_t i, j, k;
+
+	for (i = 0; i < 1024; i++) {
+		memcpy(&V[i * 32], X, 128);
+		xor_salsa8_org(&X[0], &X[16]);
+		xor_salsa8_org(&X[16], &X[0]);
+	}
+	for (i = 0; i < 1024; i++) {
+		j = 32 * (X[16] & 1023);
+		for (k = 0; k < 32; k++)
+			X[k] ^= V[j + k];
+		xor_salsa8_org(&X[0], &X[16]);
+		xor_salsa8_org(&X[16], &X[0]);
+	}
+}
 
