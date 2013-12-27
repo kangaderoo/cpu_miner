911b446a6e64c971bcfef08fb082551465c6df9e
Gerko de Roo <gerko.deroo@kangaderoo.nl>
2013-12-27 22:40:37 +0100

    inserted scrypt required SHA code in this c file for full sidm convertsion

--- salsa_20_sidm_bck.c
+++ salsa_20_sidm_bck.c
@@ -24,6 +24,7 @@
  * SUCH DAMAGE.
  */
 
+
 #include <immintrin.h>
 #include "cpuminer-config.h"
 #include "miner.h"
@@ -31,10 +32,64 @@
 #include <stdlib.h>
 #include <inttypes.h>
 
-#if defined(__x86_64__)
+
+static const uint32_t sha256_h_sidm[8] = {
+	0x6a09e667, 0xbb67ae85, 0x3c6ef372, 0xa54ff53a,
+	0x510e527f, 0x9b05688c, 0x1f83d9ab, 0x5be0cd19
+};
+
+static const uint32_t sha256_k_sidm[64] = {
+	0x428a2f98, 0x71374491, 0xb5c0fbcf, 0xe9b5dba5,
+	0x3956c25b, 0x59f111f1, 0x923f82a4, 0xab1c5ed5,
+	0xd807aa98, 0x12835b01, 0x243185be, 0x550c7dc3,
+	0x72be5d74, 0x80deb1fe, 0x9bdc06a7, 0xc19bf174,
+	0xe49b69c1, 0xefbe4786, 0x0fc19dc6, 0x240ca1cc,
+	0x2de92c6f, 0x4a7484aa, 0x5cb0a9dc, 0x76f988da,
+	0x983e5152, 0xa831c66d, 0xb00327c8, 0xbf597fc7,
+	0xc6e00bf3, 0xd5a79147, 0x06ca6351, 0x14292967,
+	0x27b70a85, 0x2e1b2138, 0x4d2c6dfc, 0x53380d13,
+	0x650a7354, 0x766a0abb, 0x81c2c92e, 0x92722c85,
+	0xa2bfe8a1, 0xa81a664b, 0xc24b8b70, 0xc76c51a3,
+	0xd192e819, 0xd6990624, 0xf40e3585, 0x106aa070,
+	0x19a4c116, 0x1e376c08, 0x2748774c, 0x34b0bcb5,
+	0x391c0cb3, 0x4ed8aa4a, 0x5b9cca4f, 0x682e6ff3,
+	0x748f82ee, 0x78a5636f, 0x84c87814, 0x8cc70208,
+	0x90befffa, 0xa4506ceb, 0xbef9a3f7, 0xc67178f2
+};
+
+static const uint32_t keypad_sidm[12] = {
+	0x80000000, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0x00000280
+};
+static const uint32_t innerpad_sidm[11] = {
+	0x80000000, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0x000004a0
+};
+static const uint32_t outerpad_sidm[8] = {
+	0x80000000, 0, 0, 0, 0, 0, 0, 0x00000300
+};
+
+
+static const uint32_t finalblk_sidm[16 * 4] __attribute__((aligned(16))) = {
+	0x00000001, 0x00000001, 0x00000001, 0x00000001,
+	0x80000000, 0x80000000, 0x80000000, 0x80000000,
+	0, 0, 0, 0,
+	0, 0, 0, 0,
+	0, 0, 0, 0,
+	0, 0, 0, 0,
+	0, 0, 0, 0,
+	0, 0, 0, 0,
+	0, 0, 0, 0,
+	0, 0, 0, 0,
+	0, 0, 0, 0,
+	0, 0, 0, 0,
+	0, 0, 0, 0,
+	0, 0, 0, 0,
+	0, 0, 0, 0,
+	0x00000620, 0x00000620, 0x00000620, 0x00000620
+};
+
 
 static inline void xor_salsa_sidm(__m128i *calc_18, __m128i *calc_13, __m128i *calc_9, __m128i *calc_7,
- 								  __m128i *calc_1, __m128i *calc_4, __m128i *calc_3, __m128i *calc_2)
+ 								  const __m128i *calc_1, const __m128i *calc_4, const __m128i *calc_3, const __m128i *calc_2)
 {
 	int i;
 	__m128i _calc;
